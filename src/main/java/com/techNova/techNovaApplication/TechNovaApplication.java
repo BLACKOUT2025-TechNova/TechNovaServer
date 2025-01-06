5fab607258dcc3d042522ba7e3d4c8cb28e31341
ji-hokim <jimmy7335@korea.ac.kr>
2025-01-06 23:23:40 +0900

    first commit

---
+++ src/main/java/com/techNova/techNovaApplication/TechNovaApplication.java
@@ -0,0 +1,13 @@
+package com.techNova.techNovaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techNova/techNovaApplication/TechNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techNova.techNovaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
